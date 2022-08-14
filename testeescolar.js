7a44e8382cfcddadab9cd6f5c9774036e18b612e
bragaboy <inaelb@gmail.com>
2022-08-14 16:46:19 -0300

    Js Notas

---
+++ testeescolar.js
@@ -0,0 +1,11 @@
+var nota = 4;
+if (nota >= 7){
+    console.log('Você foi aprovado')
+} else if (nota >= 5 && nota <= 6){
+    console.log('Você está de recuperação')
+} else if (nota <= 4){
+    console.log('Você está Reprovado')
+} else {
+    console.log('Valor não especificado')
+}
+
